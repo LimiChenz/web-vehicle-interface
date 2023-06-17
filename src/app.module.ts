468ab6ecaa3e9a1a82d4de0480bb398997204b06
chenzhirong <chenzhirong@wook.id>
2023-06-17 14:00:30 +0800

    feat: 增加mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { IndexcModule } from './module/index/index.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './module/user/user.module';
 
 @Module({
-  imports: [IndexcModule],
+  imports: [
+    IndexcModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3311,
+      username: 'root',
+      password: '/np&uYDjz3&*',
+      database: 'chen_db',
+      entities: [],
+      retryDelay: 500,
+      retryAttempts: 10,
+      autoLoadEntities: true,
+      synchronize: true, //development 开启 production 关闭
+    }),
+  ],
 })
 export class ApplicationModule {}
