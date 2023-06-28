4bf0ecd13bd3f10511fe40bb8416dcc19fb7028a
chenzhirong <chenzhirong@wook.id>
2023-06-28 21:42:48 +0800

    fix: 修复bug

--- src/module/user/user.module.ts
+++ src/module/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { LoggerMiddleware } from 'src/common/middleware/logger.middleware';
+import { LoggerMiddleware } from 'src/common/middleware/request.middleware';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
